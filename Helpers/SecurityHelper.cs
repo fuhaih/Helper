6299da29017e4671e18e4c224107579f27aa3d4a
fuhaih <1154590036@qq.com>
2016-10-26 21:13:05 +0800

    update SecurityHelper

--- Helpers/SecurityHelper.cs
+++ Helpers/SecurityHelper.cs
@@ -11,33 +11,19 @@ namespace Helpers
 {
     public static class SecurityHelper
     {
-        //public static string ToMD5<T>(this T t)
-        //{
-        //    string result = "";
-        //    byte[] buffer;
-        //    if(typeof(T)==typeof(String))
-        //    {
-        //        buffer = Encoding.Default.GetBytes(t as string);
-        //    }
-        //    else if (typeof(T) == typeof(char[]))
-        //    {
-        //        buffer = Encoding.Default.GetBytes(t as char[]);
-        //    }
-        //    else {
-        //        buffer = new byte[1024];
-        //    }
-        //    //else {
-        //    //    buffer = t.BinarySerialize();
-        //    //}
-        //    MD5 md5 = new MD5CryptoServiceProvider();
-        //    byte[] retVal = md5.ComputeHash(buffer);
-        //    StringBuilder sb = new StringBuilder();
-        //    for (int i = 0; i < retVal.Length; i++)
-        //    {
-        //        sb.Append(retVal[i].ToString("x2"));
-        //    }
-        //    result = sb.ToString();
-        //    return result;
-        //}
+        public static string ToMD5(this string t)
+        {
+            string result = "";
+            byte[] buffer= Encoding.Default.GetBytes(t as string);
+            MD5 md5 = new MD5CryptoServiceProvider();
+            byte[] retVal = md5.ComputeHash(buffer);
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < retVal.Length; i++)
+            {
+                sb.Append(retVal[i].ToString("x2"));
+            }
+            result = sb.ToString();
+            return result;
+        }
     }
 }
