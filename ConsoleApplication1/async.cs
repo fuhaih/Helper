b1f49617d590334aaea63457a06fbef38be3546f
fuhaih <1154590036@qq.com>
2017-03-11 15:59:37 +0800

    add git helper

---
+++ ConsoleApplication1/async.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public class async
+    {
+        public static async Task<int> radom() {
+            Random random = new Random();
+            await Task.Run(()=> random.Next());
+            return 1;
+        }
+
+    }
+}
