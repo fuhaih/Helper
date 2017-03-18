ba1868e0b5701fd2a96e068072cc398c7d852eda
Fuhai <Fuhai@DESKTOP-05DJGKG>
2017-03-18 12:32:28 +0800

    change framwork from 4.5 to 4

---
+++ FHORM/FHLoger.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FHLog
+{
+    public class FHLoger:IFHLoger
+    {
+        void IFHLoger.Info()
+        {
+
+        }
+
+        void IFHLoger.Warn()
+        {
+
+        }
+
+        void IFHLoger.Error()
+        {
+
+        }
+
+        void IFHLoger.Fatal()
+        {
+
+        }
+    }
+}
---
+++ FHORM/IFHLoger.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FHORM
+{
+    public interface IFHLoger
+    {
+        /// <summary>
+        /// 消息
+        /// </summary>
+        void Info();
+        /// <summary>
+        /// 警告
+        /// </summary>
+        void Warn();
+        /// <summary>
+        /// 异常
+        /// </summary>
+        void Error();
+        /// <summary>
+        /// 错误
+        /// </summary>
+        void Fatal();
+    }
+}
