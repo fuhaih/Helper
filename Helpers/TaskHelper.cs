539ee50ebb8305f29a8edad52d55cc3866236b11
fuhaih <1154590036@qq.com>
2018-12-04 18:11:31 +0800

    edit taskhelper

--- Helpers/TaskHelper.cs
+++ Helpers/TaskHelper.cs
@@ -30,8 +30,17 @@ namespace Helpers
         {
             return new ExcetpionTaskBuilder<T>(task, catchfunc);
         }
+
+        public static ExcetpionTaskReturnBuilder<T> CatchReturn<T>(this Task<T> task, Func<Exception, T> catchfunc)
+        {
+            return new ExcetpionTaskReturnBuilder<T>(task, catchfunc);
+        }
     }
 
+    /// <summary>
+    /// 捕获异常，该异步没有返回值
+    /// 所以捕获到异常后是继续向上抛出还是处理异常，由回调函数决定
+    /// </summary>
     public class ExcetpionTaskBuilder
     {
         private Action<Exception> CatchAction;
@@ -58,6 +67,12 @@ namespace Helpers
         }
     }
 
+    /// <summary>
+    /// 捕获异常，会重新抛出异常
+    /// 常用场景：进度条。
+    /// 某个异步操作前启动进度条，如果异步操作异常，需要先隐藏进度条，再向上抛出异常
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
     public class ExcetpionTaskBuilder<T>
     {
         private Func<Exception, Exception> CatchFunc;
@@ -83,5 +98,34 @@ namespace Helpers
             }
         }
 
+    }
+    /// <summary>
+    /// 捕获异常，并返回一个默认值
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class ExcetpionTaskReturnBuilder<T>
+    {
+        private Func<Exception, T> CatchFunc;
+        private Task<T> CatchTask;
+        public ExcetpionTaskReturnBuilder(Task<T> task, Func<Exception, T> catchFunc)
+        {
+            this.CatchTask = task;
+            this.CatchFunc = catchFunc;
+        }
+        public async Task<T> Finally(Action finallyAction = null)
+        {
+            try
+            {
+                return await CatchTask;
+            }
+            catch (Exception ex)
+            {
+                return CatchFunc(ex);
+            }
+            finally
+            {
+                finallyAction?.Invoke();
+            }
+        }
     }
 }
